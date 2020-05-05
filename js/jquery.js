f0747fb3a35869af62e8832f87a64123383b7006
RemAlw <nhoxtvt@gmail.com>
2020-05-05 17:32:56 +0700

    home-page

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,4 @@
+////Setting
 $(document).ready(function() {
 
     var active1 = false;
@@ -22,3 +23,130 @@ $(document).ready(function() {
         
       });
   });
+
+
+////Time
+function clock(){
+  var hours = document.getElementById('hour');
+  var minutes = document.getElementById('minutes');
+  var seconds = document.getElementById('seconds');
+  
+  var h = new Date().getHours();
+  var m = new Date().getMinutes();
+  var s = new Date().getSeconds();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+}
+  var interval = setInterval(clock,1000);
+ 
+////Wait
+
+  // var waitsale = document.getElementById("waitsale")
+  // window.addEventListener("scroll",function() {
+  //   waitsale.style.left = +window.pageYOffset + "px";
+  // })
+
+////Waiting
+// $(document).ready(function(){
+//   $(window).scroll(function(event){
+//       var head = $('html,body').scrollTop();
+//       var lock = $('.sale').offset().top;
+//       console.log(lock);
+//     //----------function
+//           if(head > lock-400){
+//             $('.time').addClass('Bouncein animated');
+//           }
+//   });
+// });
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     $('.nav-bg').css("opacity",1 - $(window).scrollTop()/500)
+//   });
+// });
+window.onload = function(){
+    const TIME =document.querySelector(".time");
+    const SALE =document.querySelector(".headland-content--text");
+    const BTN =document.querySelector(".btn-box");
+    window.addEventListener('scroll',scrolltime);
+    window.addEventListener('scroll',scrollsale);
+    function scrolltime(){
+        if(window.scrollY >=750) {
+          TIME.style.opacity = '1';
+          TIME.style.transform = 'translateX(0px)';
+          TIME.style.transition = '1s ease-in-out';
+          TIME.style.transition = '2s ease-in-out';
+        }
+        else
+        {
+          TIME.style.opacity = '0';
+          TIME.style.transform = 'translateX(-10rem)';
+        }
+    }
+    function scrollsale(){
+      if(window.scrollY >=400) {
+        SALE.style.opacity = '1';
+        SALE.style.transform = 'translateY(0px)';
+        SALE.style.transition = '1s ease-in-out';
+      }
+      else
+      {
+        SALE.style.opacity = '0';
+        SALE.style.transform = 'translateY(-10rem)';
+      }
+  }
+   
+};
+
+////BTN-OPTION
+
+// var h1 = document.querySelector("btn-option");
+
+// h1.addEventListener("input", function() {
+//   this.setAttribute("data-heading", this.innerText);
+// });
+// var h1 = document.querySelector("sidebar-product--link");
+
+// h1.addEventListener("input", function() {
+//   this.setAttribute("data-sidebar", this.innerText);
+// });
+//---------
+//======OVERLAY
+gsap.to('.taitrang-1',{
+  duration :2,
+  delay:.2,
+  ease: "steps.InOut",
+  width:0
+})
+gsap.to('.taitrang-2',{
+  duration :1.5,
+  delay:.4,
+  ease: "bounce.InOut",
+  width:0
+})
+gsap.to('.taitrang-3',{
+  duration :1.5,
+  delay:.6,
+  ease: "bounce.InOut",
+  width:0
+})
+gsap.to('.taitrang-4',{
+  duration :1.5,
+  delay:.8,
+  ease: "bounce.InOut",
+  width:0
+})
+gsap.to('.taitrang-5',{
+  duration :1.5,
+  delay:1,
+  ease: "bounce.InOut",
+  width:0
+})
+gsap.from('.btn-box',{
+  duration :1.5,
+  delay:1,
+  ease: "steps.InOut",
+  y:-1000
+})
+//===============
